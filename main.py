b6162f552d90a19c56e47bcbdbbf124ca594671b
gerstnir <gernir@gmail.com>
2026-04-29 15:23:31 +0300

    changed content for escape character lecture

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-print("python")
+ex1 = "prepare the people\'s \"laser\" now !"
+ex2 = "we can meet today\\tomorrow"
+ex3 = "i am a\nnew-line !"
+ex4 = "one two three \rfour"
+ex5 = "i am close but\t\t\ti am far away!"
+ex6 = "can't see this\b\b\b\b\b HAHA"
 
-print("I love python")
-
-print('I love "python"')
-
-print('"smart" people use "python"')
+print(ex1, ex2, ex3, ex4, ex5, ex6)
